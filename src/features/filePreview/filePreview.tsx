41621f623f99e45ba45bb593e59c24cfffe2a8bd
Manish <m.hyongoju04@gmail.com>
2025-10-28 17:49:25 +0545

    feat: file preview according to file type

--- src/features/filePreview/filePreview.tsx
+++ src/features/filePreview/filePreview.tsx
@@ -1,5 +1,33 @@
+'use client'
 import React from "react";
+import {motion} from "framer-motion"
 
-export default function FilePreview() {
-  return <div>This is file preview folder</div>;
+interface FilePreviewProps {
+    file: File,
+    onRemove?: () => void
 }
+
+export const FilePreview = React.forwardRef<HTMLDivElement, FilePreviewProps>(
+  (props, ref) => {
+    if(props.file.type.startsWith("image/")) {
+        return <ImageFilePreview {...props} ref={ref} />
+    }
+    if(props.file.type.startsWith("text/") || props.file.type.startsWith(".txt") || props.file.type.startsWith(".md")){
+        return <TextFilePreview {...props} ref={ref} />
+    }
+
+    return <GenericFilePreview {...props} ref={ref} />
+}
+)
+
+FilePreview.displayName = "FilePreview"
+
+
+
+const ImageFilePreview =React.forwardRef<HTMLDivElement, FilePreviewProps>(({file, onRemove}, ref) => {
+return (
+    <motion.div>
+
+    </motion.div>
+)
+})
